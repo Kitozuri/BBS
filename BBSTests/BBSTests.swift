fa95eacc72c4f0d50df5479274b216639f010555
Kitozuri <nnguyenhoanggiahuy.71@gmail.com>
2024-10-05 19:12:49 +0700

    Initial Commit

---
+++ BBSTests/BBSTests.swift
@@ -0,0 +1,17 @@
+//
+//  BBSTests.swift
+//  BBSTests
+//
+//  Created by Nguyễn Hoàng Gia Huy on 5/10/24.
+//
+
+import Testing
+@testable import BBS
+
+struct BBSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
