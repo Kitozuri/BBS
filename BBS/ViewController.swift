66fe1a3815685e3252c96830811835dc21c50164
Kitozuri <nnguyenhoanggiahuy.71@gmail.com>
2024-10-05 19:32:06 +0700

    đư

--- BBS/ViewController.swift
+++ BBS/ViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
     override func viewDidLoad() {
+        print("kh")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
